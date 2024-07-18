b232ba1090df1741288bd85828cb9235a26811b5
Hieu Pham <hieu.pham@employmenthero.com>
2024-07-18 13:53:20 +0700

    Update with explanation of z

--- 3-yz-combinators.js
+++ 3-yz-combinators.js
@@ -9,8 +9,15 @@ const fact_gen = (f) => (n) => {
 // Idea: SA(SA)
 const Idea = ((g) => g(g))((g) => g(g));
 
-// Fixed point recursion: Add f for stoping
+// Fixed point recursion of f: f(f(f(f(...))))
+// Fixed point of f mean:
+// - if f(x) = x then every x is a fixed point of f
 const Y = (f) => ((g) => f(g(g)))((g) => f(g(g)));
 
-// Because of Applicative order in JS, fix it by introducing n
+// Y(fact_gen)(2) still raises exception in JS
+// Because it requires evaluating g(g) itself
+// We know fact(1) doesn't need any recursion
+// Instead of call an infinite series of fact functions
+// Just call it when we need it by Lazy evaluation
+// n => g(g)(n)
 const Z = (f) => ((g) => f((n) => g(g)(n)))((g) => f((n) => g(g)(n)));
